0a5c3c5dc2bf41a9e9dab1e63ec1d11800d4257b
ungalexy <ungalexy@gmail.com>
2018-07-04 20:56:35 +0300

    progress collection pattern map docs

---
+++ app/Processes/_readme.php
@@ -0,0 +1,46 @@
+<?php 
+
+
+
+#######################################
+# Processes
+#######################################
+
+
+
+/**
+ * --------------------------------------------------------------------------
+ *  Locator
+ * --------------------------------------------------------------------------
+ * 
+ * Locat "Prospect Items" from given "Prospects Channels" & store them in "Propects Q" for forther handling by the Watcher.
+ * 
+ * TODO: Ali adapter
+ * 
+ */ 
+
+
+ /**
+ * --------------------------------------------------------------------------
+ *  Watcher
+ * --------------------------------------------------------------------------
+ * 
+ * The Watcher whatching the prospect items, tracking, comparing & mark their performance according to set of conditions across mutiple channels.
+ * 
+ * TODO: write the conditions
+ * 
+ */ 
+
+
+ /**
+ * --------------------------------------------------------------------------
+ *  Realizer
+ * --------------------------------------------------------------------------
+ * 
+ * The Realizer collect, organise & transform the data into api stactured records.
+ * 
+ */ 
+
+
+
+
