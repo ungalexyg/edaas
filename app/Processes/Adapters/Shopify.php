967c46a493dd08cf99ce7e89a74db827b3dd3ad4
ungalexy <ungalexy@gmail.com>
2018-07-28 19:19:40 +0300

    site search sample

--- app/Processes/Adapters/Shopify.php
+++ app/Processes/Adapters/Shopify.php
@@ -11,6 +11,7 @@
  * https://www.allithypermarket.com.my/collections/drones-accessories?sort_by=best-selling
  * https://www.google.co.il/search?q=site%3Amyshopify.com%2Fcollections%2Fsunglasses&oq=site%3Amyshopify.com%2Fcollections%2Fsunglasses&aqs=chrome..69i57j69i58j69i59.700j0j9&sourceid=chrome&ie=UTF-8
  * 
+ * site:myshopify.com: search
  */
 
 
