a9666a095955b642a0dd1c7ca41279bf83338445
ungalexy <ungalexy@gmail.com>
2018-08-17 17:49:09 +0300

    add laravel snip

---
+++ app/Lib/Snip/Laravel/laravel-snip.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+#######################################
+# DBG Query 
+########################################
+
+use Illuminate\Support\Facades\DB;
+DB::enableQueryLog();
+...
+$qlog = DB::getQueryLog();dd($qlog);
