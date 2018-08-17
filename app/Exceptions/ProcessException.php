c8ea807d67e545eb3b66390f37b4a907e10f4412
ungalexy <ungalexy@gmail.com>
2018-08-17 06:58:57 +0300

    add push/pull exceptions

--- app/Exceptions/ProcessException.php
+++ app/Exceptions/ProcessException.php
@@ -10,12 +10,16 @@ use Exception;
  */
 class ProcessorException extends BaseException
 {
-    const PROCESSOR_UNDEFINED           = 'Trying to load undefined processor';
-    const PROCESSOR_SCANNER_UNDEFINED   = 'Trying to load undefined scanner';
-    const PROCESSOR_KEEPER_UNDEFINED    = 'Trying to load undefined keeper';
-    const PROCESSOR_WATCHER_UNDEFINED   = 'Trying to load undefined watcher';  
-    const PROCESSOR_PROCESS_UNDEFINED   = 'Trying to set undefined process';
-    const PROCESSOR_CHANNEL_UNDEFINED   = 'Trying to set undefined channel';
+    const UNDEFINED_PROCESS             = 'Can\'t set undefined process key';
+    const UNDEFINED_CHANNEL             = 'Can\'t set undefined channel key';
+    const UNDEFINED_PROCESSOR           = 'Failed to load undefined processor instance';
+    const UNDEFINED_SCANNER             = 'Failed to load undefined scanner instance';
+    const UNDEFINED_KEEPER              = 'Failed to load undefined keeper instance';
+    const UNDEFINED_WATCHER             = 'Failed to load undefined watcher instance';  
+    const FAILED_PULL_INVALID_INSTANCE  = 'Trying to pull process data from invalid processor instance';
+    const FAILED_PUSH_INVALID_INSTANCE  = 'Trying to push process data to invalid processor instance';
+    const FAILED_PULL_INVALID_PROPERTY  = 'Trying to pull process data from invalid processor property';
+    const FAILED_PUSH_INVALID_PROPERTY  = 'Trying to push process data from invalid property on the pushing instance';
 }
 
 
