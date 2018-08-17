238d1a845b8a18e72e0b1839d72bbecd243389be
ungalexy <ungalexy@gmail.com>
2018-08-17 17:52:39 +0300

    add local scope - get mature channels to process

--- app/Models/Process.php
+++ app/Models/Process.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use App\Models\Channel;
+use Illuminate\Support\Carbon;
 
 
 /**
  * Process Model
- * 
  */
 class Process extends BaseModel
 {
@@ -37,11 +37,46 @@ class Process extends BaseModel
 
 
     /**
+     * Fileds constants
+     */
+    const LAST_PROCESS = 'last_process';
+
+
+    /**
      * Get the channels that attached to the process
      */
     public function channels()
     {
-        return $this->belongsToMany(Channel::class, 'channels_processes', 'process_id', 'channel_id')->withPivot('last_process');
+        return $this->belongsToMany(Channel::class, 'channels_processes', 'process_id', 'channel_id')->withPivot(static::LAST_PROCESS);
     }    
 
+
+    /**
+     * Scope mature channels processes
+     * 
+     * @see config('processes.settings.categories')
+     * @param Illuminate\Database\Query\Builder // injected natively
+     * @param string $process // processes.key
+     * @return void
+     */
+    public function scopeMatureChannels($query, $process)
+    {
+        $config = config('processes.settings.categories');
+
+        $min_age = $config['min_age'] ?? (60*24); // 24 hours in minutes
+        
+        $limit_channels = $config['limit_channels'] ?? 1; 
+        
+        $datetime = Carbon::now()->subMinutes($min_age)->toDateTimeString();
+
+        $query->with(['channels' => function($q) use ($datetime, $limit_channels){
+
+            $q->wherePivot(static::LAST_PROCESS,'<=', $datetime)
+            ->orderBy('channels_processes.'.static::LAST_PROCESS, 'asc')
+            ->take($limit_channels);
+
+        }])->where('key', $process);   
+        
+    }    
+ 
 }
