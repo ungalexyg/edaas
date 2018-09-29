4809cd1d6f2d26b339f797cb2936680914573c9f
ungalexy <ungalexy@gmail.com>
2018-09-29 13:10:41 +0300

    prep g trends trait

---
+++ app/Processes/Adapters/Google/Traits/GTrends.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Processes\Adapters\Google\Traits;
+
+
+/**
+ * Google Trends
+ * 
+ * TODO: https://github.com/jonasva/laravel-google-trends
+ * 
+ */
+trait GTrends 
+{
+	
+
+}
